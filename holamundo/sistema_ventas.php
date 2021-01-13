d4ab9b8b55fd6dfdcd7c847f397ef2fe41592319
Aldair199 <aldairramosvasquez@gmail.com>
2021-01-13 11:06:14 -0500

    enviando nuevos cambios

--- holamundo/sistema_ventas.php
+++ holamundo/sistema_ventas.php
@@ -7,7 +7,7 @@
  
  <?php
  
- echo "<h1><center>¡Hola mundo este es ! <br></center><h1>";
+ echo "<h1><center>¡Hola mundo este es  un nuevo proyecto de aldair! <br></center><h1>";
   echo "<h2>  mi proyecto de sistema de ventas de programacion web </h2>";
  
  ?>
